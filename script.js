1b14deb89b66b9cec8ad35cad9a2663612149e57
Bielzin125 <gabrielcandidogabriel001@gmail.com>
2024-06-01 10:14:38 -0300

    win11demo

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const button = document.querySelector('#botao');
-document.addEventListener('click', function(event){
-    document.querySelector('#container01')
-        .classList.toggle('fade');
-        
-
+document.addEventListener('DOMContentLoaded', function() {
+    const botao = document.querySelector('#botao');
+    botao.addEventListener('click', function(evento) {
+        document.querySelector('#container01').classList.toggle('fade');
+    });
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pesquisar = document.querySelector('#pesquisa');
+    pesquisar.addEventListener('click', function(evento) {
+        document.querySelector('#pesquisar').classList.toggle('fade');
+    });
+});
+
+
 function spot() {
-    window.location.href = 'spotfy.html'
+    window.location.href = 'spotfy.html';
+}
+
+function cmd() {
+    window.location.href = 'comand.html';
 }
